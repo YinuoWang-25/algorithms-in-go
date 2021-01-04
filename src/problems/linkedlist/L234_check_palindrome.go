cc816717532ef4e5292c1382d15e843b38408d48
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-01-03 23:48:44 -0800

    Palindrome list

---
+++ src/problems/linkedlist/L234_check_palindrome.go
@@ -0,0 +1,51 @@
+package linkedlist
+
+import "algorithms-in-go/src/problems/base"
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil {
+		return true
+	}
+	first := head
+	middle := findMiddle(head)
+	second := middle.Next
+	middle.Next = nil
+	second = (*base.ListNode)(reverse((*ListNode)(second)))
+	for first != nil && second != nil {
+		if first.Val != second.Val {
+			return false
+		}
+		first = (*ListNode)(first.Next)
+		second = second.Next
+	}
+	if first != nil {
+		first = (*ListNode)(first.Next)
+	}
+	return first == nil && second == nil
+}
+
+func findMiddle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	slow := head
+	fast := head
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = (*ListNode)(slow.Next)
+		fast = (*ListNode)(fast.Next.Next)
+	}
+	return slow
+}
+
+func reverse(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = (*base.ListNode)(prev)
+		prev = cur
+		cur = (*ListNode)(next)
+	}
+	return prev
+}
