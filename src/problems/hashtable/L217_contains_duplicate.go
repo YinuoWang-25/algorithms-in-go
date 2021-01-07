97cd60d6883447164daa99b05b286de98ffee522
YinuoWang-25 <71306986+YinuoWang-25@users.noreply.github.com>
2021-01-06 22:12:22 -0800

    Contains Duplicate

---
+++ src/problems/hashtable/L217_contains_duplicate.go
@@ -0,0 +1,14 @@
+package hashtable
+
+func containsDuplicate(nums []int) bool {
+
+	hash := make(map[int]bool)
+
+	for i := 0; i < len(nums); i++ {
+		if _, ok := hash[nums[i]]; ok {
+			return true
+		}
+		hash[nums[i]] = true
+	}
+	return false
+}
